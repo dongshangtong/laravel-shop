96b303f8f0eac8e0f316b3350a6feb5042bb0b1a
dongshangtong <dongshangtong@gmail.com>
2018-09-04 02:51:50 +0800

    发布

---
+++ deploy.php
@@ -0,0 +1,43 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'laravel-shop');
+
+// Project repository
+set('repository', 'git@github.com:dongshangtong/laravel-shop.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+
+set('branch', 'master'); 
+// Hosts
+
+host('120.24.241.188')
+    ->user('deployer') // 这里填写 deployer
+      // 并指定公钥的位置
+    ->identityFile('~/.ssh/deployerkey.pub')
+
+    ->set('deploy_path', '~/var/www/laravel-shop'
+
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
