0daaf803a53cf1dd824f68ea9c7dbc383c04778e
dongshangtong <dongshangtong@gmail.com>
2018-09-04 02:57:33 +0800

    fdsfadks

--- deploy.php
+++ deploy.php
@@ -5,39 +5,20 @@ require 'recipe/laravel.php';
 
 // Project name
 set('application', 'laravel-shop');
-
+set('cleanup_use_sudo', true);
+set('writable_use_sudo', false);
+set('branch', 'master');    // 发布代码时候默认使用的分支
+set('shared_files', []);    // 共享文件列表   这里面列出的文件会被移动到项目根目录的shared目录下，并做软链
+set('shared_dirs', []);     // 共享目录    同上
+set('writable_mode', 'chmod');  // 采用哪种方式控制可写权限，有4中：chown、chgrp、chmod、acl（默认方式）
+set('writable_chmod_mode', '0755'); // 当使用chmod控制可写权限的时候，赋予的可写权限值
+set('writable_dirs', []);
 // Project repository
 set('repository', 'git@github.com:dongshangtong/laravel-shop.git');
-
-// [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true);
-
-// Shared files/dirs between deploys
-add('shared_files', []);
-add('shared_dirs', []);
-
-// Writable dirs by web server
-add('writable_dirs', []);
-
-set('branch', 'master');
-// Hosts
 
 host('120.24.241.188')
     ->user('deployer') // 这里填写 deployer
       // 并指定公钥的位置
     ->identityFile('~/.ssh/deployerkey.pub')
 
-    ->set('deploy_path', '~/var/www/laravel-shop'
-
-// Tasks
-
-// task('build', function () {
-//     run('cd {{release_path}} && build');
-// });
-
-// [Optional] if deploy fails automatically unlock.
-// after('deploy:failed', 'deploy:unlock');
-
-// Migrate database before symlink new release.
-
-// before('deploy:symlink', 'artisan:migrate');
+    ->set('deploy_path', '~/var/www/laravel-shop');
