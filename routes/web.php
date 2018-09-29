788d77c47f19aa4795f4d1576635fabd27197bad
vagrant <vagrant@homestead>
2018-09-29 23:25:13 +0000

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,9 @@ Route::group(['middleware' => 'auth'] ,function ($value='')
 
         // 开始
         Route::group(['middleware' => 'email_verified'], function() {
-        
+
+            Route::get('user_addresses','UserAddressesController@index')->name('user_addresses.index');
+
         });
 // 结束
 });
