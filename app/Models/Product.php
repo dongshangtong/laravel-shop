a33cdff21a005c2a3d3f71591523e85981945e3c
vagrant <vagrant@homestead>
2018-10-10 08:31:57 +0000

    用户界面 - 商品列表

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\ProductSku;
+use Illuminate\Support\Str;
 
 
 class Product extends Model
@@ -23,5 +24,15 @@ public function skus()
 {
   return $this->hasMany(ProductSku::class);
 }
+
+
+public function getImageUrlAttribute()
+  {
+      // 如果 image 字段本身就已经是完整的 url 就直接返回
+      if (Str::startsWith($this->attributes['image'], ['http://', 'https://'])) {
+          return $this->attributes['image'];
+      }
+      return \Storage::disk('public')->url($this->attributes['image']);
+  }
 
 }
