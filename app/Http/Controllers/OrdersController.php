85a6fa08c2ffb4f42a13d2d1e11d7492521c6356
vagrant <vagrant@homestead>
2018-10-18 13:08:08 +0000

    用户界面 - 订单详情页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -83,4 +83,16 @@ public function index(Request $request)
 
      return view('orders.index', ['orders' => $orders]);
  }
-}
+
+
+ public function show(Order $order, Request $request)
+ {
+          $this->authorize('own', $order);
+
+     return view('orders.show', ['order' => $order->load(['items.productSku', 'items.product'])]);
+ }
+
+
+
+
+ }
