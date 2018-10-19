931266e37849e15c451ae473538559e820e98eb6
vagrant <vagrant@homestead>
2018-10-19 10:03:57 +0000

    管理后台 - 同意退款 - 微信支付退款

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -23,5 +23,6 @@ Route::group([
     $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
     $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
     $router->post('orders/{order}/refund','OrdersController@handleRefund')->name('admin.orders.handle_refund');
+    Route::post('payment/wechat/refund_notify','PaymentController@wechatRefundNotify')->name('payment.wechat.refund_notify');
 
 });
