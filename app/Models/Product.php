393b78e09f96f469775096a217f419faf79c2a68
vagrant <vagrant@homestead>
2018-10-22 12:45:04 +0000

    商品属性模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\ProductSku;
+use App\Models\ProductProperty;
 use App\Models\Category;
 use Illuminate\Support\Str;
 
@@ -51,9 +52,28 @@ public function getImageUrlAttribute()
 
 
 
+  public function getGroupedPropertiesAttribute()
+{
+    return $this->properties
+        // 按照属性名聚合，返回的集合的 key 是属性名，value 是包含该属性名的所有属性集合
+        ->groupBy('name')
+        ->map(function ($properties) {
+            // 使用 map 方法将属性集合变为属性值集合
+            return $properties->pluck('value')->all();
+        });
+}
+
+
+
 public function category()
 {
     return $this->belongsTo(Category::class);
 }
+
+public function properties()
+{
+  return $this->hasMany(ProductProperty::class);
+}
+
 
 }
