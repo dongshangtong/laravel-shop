1d07a205e5d28e64bcd21bf4dc44804dc5850a7e
dongshangtong <dongshangtong@gmail.com>
2020-01-20 21:41:29 +0800

    备案号

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,6 +3,7 @@
         <p class="pull-left">
             由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">dongshangtong</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
+              <p class="pull-contenr">© 2018 - 2020<a href="http://beian.miit.gov.cn/" target="_blank">粤ICP备15013243号-1 </a></p>
 
         <p class="pull-right"><a href="dongshangtong@gmail.com">联系我们</a></p>
     </div>
